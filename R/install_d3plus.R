4098dd4629bada9f2cc20689bb48f192cc4155ff
Mauricio Vargas S <mauriciovargas@ug.uchile.cl>
2016-08-15 15:50:46 +0000

    version 2.0

--- R/install_d3plus.R
+++ R/install_d3plus.R
@@ -1,12 +1,12 @@
-#' Installs D3Plus
+#' Installs D3 and D3plus
 #' @export
 #' @return Copies a folder named \code{d3plus} to the working directory and it contains the \code{js} files and icons to make the visualizations
 #' @importFrom utils unzip
 #' @examples
-#' d3plus()
+#' install_d3plus()
 #' @keywords functions
 
-d3plus <- function() {
+install_d3plus <- function() {
   file.copy(from=system.file("extdata", "d3plus.zip", package = "oec"), to=getwd())
   unzip("d3plus.zip")
   file.remove("d3plus.zip")
